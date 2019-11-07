f10b0737c225ef2aabeed9eac71bfe00a7e084bf
epwalsh <epwalsh10@gmail.com>
2019-11-07 09:28:27 -0800

    use standalone gunicorn app

--- mallennlp/wsgi.py
+++
@@ -1,7 +0,0 @@
-from mallennlp.app import create_app, create_dash
-from mallennlp.services.config import Config
-
-
-config = Config.from_toml()
-application = create_app(config)
-dashboard = create_dash(application, config)
